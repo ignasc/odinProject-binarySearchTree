8da1ef8695ae7786e8eaad3417dc0d342f2de50d
Ignas <15948796+ignasc@users.noreply.github.com>
2025-09-29 09:25:03 +0100

    Add tree class template.

---
+++ classTree.js
@@ -0,0 +1,26 @@
+class Tree {
+    constructor(array){
+        this.root = this.buildTree(array);
+    }
+
+    buildTree(array){
+        return null
+    }
+
+    //function that outputs a visual representation of the tree in console
+    prettyPrint = (node, prefix = '', isLeft = true) => {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    };
+
+}
+
+export default Tree;
